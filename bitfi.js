8a46938c5e8cfb17e2a3af3d26da7fb789f9415a
just4programming <sergey@bitfi.com>
2022-05-20 19:03:20 -0300

    save

---
+++ bitfi.js
@@ -0,0 +1,213 @@
+const WebSocket = require('ws')
+const base64 = require('base64-js')
+const CryptoJS = require('crypto')
+const fetch = require('node-fetch')
+const ecdsa = require('secp256k1')
+const bs58 = require('./bs58')
+
+function hexToBytes(hex) {
+  for (var bytes = [], c = 0; c < hex.length; c += 2)
+      bytes.push(parseInt(hex.substr(c, 2), 16));
+  return bytes;
+}
+
+function calculateCode(randomSigningData, privKey, deviceId) {
+  const pubKey = Buffer.from(ecdsa.publicKeyCreate(Buffer.from(privKey, 'hex'), true))
+  const ripemd160 = CryptoJS.createHash('ripemd160')
+  const sha256 = CryptoJS.createHash('sha256')
+  const md5 = CryptoJS.createHash('md5')
+
+  const key160 = ripemd160.update(sha256.update(pubKey).digest()).digest()
+
+  const data = Buffer.concat([
+    key160,
+    Buffer.from(deviceId, 'hex'),
+    Buffer.from(randomSigningData, 'hex')
+  ])
+  
+  const md5hash = md5.update(data).digest().toString('hex')
+
+  const tmp = md5hash.slice(-12)
+  const first = bs58.encode(hexToBytes(tmp.slice(0, 4)))
+  const second = tmp.slice(4, 10)
+  
+  return `${first}-${second}`.toUpperCase()
+}
+
+class Bitfi {
+  constructor(
+    authToken, 
+    publicKey, 
+    sessionSecret, 
+    address, 
+    config
+  ) {
+    this._config = config
+    this._authToken = authToken
+    this._publicKey = publicKey
+    this._address = address,
+    this._sessionSecret = sessionSecret
+  }
+
+  getPublicKey() {
+    return this._publicKey
+  }
+
+  getAddress() {
+    return this._address
+  }
+
+  getAuthToken() {
+    return this._authToken
+  }
+
+  getConfig() {
+    return this._config
+  }
+
+  getSessionSecret() {
+    return this._sessionSecret
+  }
+
+  receiveEnvoy(envoyToken, onMessage) {
+    return new Promise((res, rej) => {
+      var websocket = new WebSocket(this._config.envoyUrl);
+      
+      websocket.on("open", function (event) {
+        websocket.send(JSON.stringify({ ClientToken: envoyToken }));
+      });
+  
+      websocket.on("message", function (e) {
+        var obj = JSON.parse(e);
+
+        const envoyMes = {
+          completed: obj.Completed,
+          error_message: obj.Error,
+          user_message: obj.Message,
+          ticks: obj.Ticks
+        } 
+
+        if (envoyMes.error_message) {
+          //websocket.close()
+          rej(envoyMes.error_message)
+        }
+
+        if (!envoyMes.completed) {
+          onMessage && onMessage(envoyMes)
+        }
+        
+        if (envoyMes.user_message && envoyMes.completed) {
+          websocket.close()
+          res(JSON.parse(Buffer.from(base64.toByteArray(envoyMes.user_message), 'hex')))
+        }
+      })
+    })
+  }
+
+  async signMessageBlind(message, onMessage) {
+    let envoyToken = ''
+    
+    const sessionSecret = Buffer.from(this._sessionSecret, 'hex')
+    const sha256 = CryptoJS.createHash('sha256')
+    const hash = sha256.update(message, 'utf8').digest()    
+    const pubKey = ecdsa.publicKeyCreate(sessionSecret)
+    const res = ecdsa.ecdsaSign(hash, sessionSecret)
+
+
+    //verify signature
+    const verified = ecdsa.ecdsaVerify(res.signature, hash, pubKey)
+
+    if (!verified) {
+      throw new Error("Signature is not valid")
+    }
+
+    const signatureDer = ecdsa.signatureExport(res.signature)
+
+    try {
+
+      const request = {
+        authToken: this._authToken,
+        method: 'BlindMessage',
+        messageModel: {
+          BlindRequest: {
+            PublicKey: Buffer.from(pubKey).toString('hex'),
+            RequestMessage: message,
+            Signature: Buffer.from(signatureDer).toString('hex')
+          },
+          MessageRequest: {
+            Address: this._address,
+            Symbol: 'dag',
+          }
+        }
+      }
+
+      const { data } = await fetch.post(this._config.apiUrl, request)
+  
+      if (data && data.error) {
+        throw new Error(data.error && data.error.message)
+      }
+  
+      if (!data && typeof data !== 'string') {
+        throw new Error("Not valid envoy token")
+      }
+  
+      envoyToken = data
+    }
+    catch (exc) {
+      throw new Error(`Unable to fetch envoy token: ${JSON.stringify(exc && exc.message)}`)
+    }
+  
+    const raw = await this.receiveEnvoy<any>(envoyToken, onMessage)
+
+    return {
+      success: raw.Success,
+      signatureResponse: raw.SignatureResponse,
+      nodeResponse: raw.NodeResponse
+    }
+  }
+  
+
+  async signMessagePrefixed(message, onMessage) {
+    let envoyToken = ''
+  
+    try {
+      const { data } = await fetch.post(this._config.apiUrl, {
+        authToken: this._authToken,
+        method: 'SignMessage',
+        messageModel: {
+          MessageRequest: {
+            Message: message,
+            Address: this._address,
+            Symbol: 'dag',
+          }
+        }
+      })
+  
+      if (data && data.error) {
+        throw new Error(data.error && data.error.message)
+      }
+  
+      if (!data && typeof data !== 'string') {
+        throw new Error("Not valid envoy token")
+      }
+  
+      envoyToken = data
+    }
+    catch (exc) {
+      throw new Error(`Unable to fetch envoy token: ${JSON.stringify(exc && exc.message)}`)
+    }
+  
+    const raw = await this.receiveEnvoy<any>(envoyToken, onMessage)
+
+    return {
+      success: raw.Success,
+      signatureResponse: raw.SignatureResponse,
+      nodeResponse: raw.NodeResponse
+    }
+  }
+}
+
+module.exports = {
+  Bitfi,
+  calculateCode
+}
---
+++ index.js
@@ -0,0 +1,235 @@
+const EventEmitter = require('events').EventEmitter
+const { Bitfi, calculateCode } =  require('./bitfi')
+const ecdsa = require('secp256k1')
+const WebSocket = require('ws')
+const fetch = require('node-fetch')
+
+function signin(params) {
+  const privKey = Buffer.from(params.appSecret, 'hex')
+  const randomSigningData = Buffer.from(params.signData, 'hex')
+  const deviceId = Buffer.from(params.deviceId, 'hex')
+  
+  if (!ecdsa.privateKeyVerify(privKey)) {
+    throw new Error("Invalid ecdsa key, please, provide another one")
+  }
+
+  if (deviceId.length !== 3) {
+    throw new Error('Invalid device ID')
+  }
+
+  if (randomSigningData.length !== 16) {
+    throw new Error("Invalid randomsigning data")
+  }
+
+  if (privKey.length !== 32) {
+    throw Error("Inavlid private key")
+  }
+
+  const pubKey = Buffer.from(ecdsa.publicKeyCreate(privKey, true))
+  const code = calculateCode(params.signData, params.appSecret, params.deviceId)
+
+  let notified = false
+
+  return new Promise((res, rej) => {
+    
+    let websocket = new WebSocket(params.url);
+    
+    const request = {
+      data_for_signing: randomSigningData.toString('hex'),
+      device_id: params.deviceId,
+      match_profile: true,
+      request_method: "register",
+      public_key: `${pubKey.toString('hex')}`, //04 means uncompressed public key
+      request_id: "",
+      derivation_index: "22" //dag
+    }
+
+    websocket.on('open', function (event) {
+      websocket.send(JSON.stringify(request));
+    });
+    
+
+    websocket.on('message', async function (e) {
+      const response = JSON.parse(e)
+
+      if (response.display_code && response.display_code !== code) {
+        rej('Codes are not equal, please, contact support')
+      }
+
+      if (response.error_message) {
+        //websocket.close()
+        rej(response.error_message)
+      }
+
+      if (response.notified && !notified) {
+        params.onNotified && params.onNotified()
+        notified = true
+      }
+
+      if (response.completed === false) {
+        params.onMessage && params.onMessage(response)
+      } else {
+        const token = response.request_id 
+
+        try {
+          const keyring = new BitfiKeyring()
+          await keyring.deserialize({
+            authToken: token,
+            publicKey: response.public_key,
+            appSecret: params.appSecret,
+            config: params.config
+          })
+          res(keyring)
+        }
+        catch (exc) {
+          rej(exc)
+        }
+        finally {
+          websocket.close()
+        }
+      }
+    });
+  })
+};
+
+async function request(token, method, url, params = undefined) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify({
+      authToken: token,
+      method,
+      transferModel: params
+    })
+  })
+  const json = await response.json()
+
+  if (json.error)
+    throw new Error(json.Content.error)
+
+  return json
+}
+
+async function init(token, public_key, secret, config) {
+  
+  const { Content : addresses } = await request(token, 'GetAddresses', config.apiUrl)
+
+  if (!addresses || !addresses[0])
+    throw new Error('No address')
+  
+  const valid = await request(token, 'IsTokenValid', config.apiUrl)
+
+  if (!valid)
+    throw new Error('Invalid token, sign in again')
+  
+  const address = addresses[0]
+  const bitfi = new Bitfi(token, public_key, secret, address, config)
+  
+  return bitfi
+  
+}
+
+class BitfiKeyring extends EventEmitter {
+  constructor(opts) {
+    super(opts)
+    this._bitfi = null
+  }
+
+  async signPersonalMessage(address, data) {
+    this._checkAddress(address)
+
+    const res = await this._bitfi.signMessagePrefixed(data)
+    return res.signatureResponse
+  }
+
+  async serialize() {
+    const authToken = this._bitfi.getAuthToken()
+    const appSecret = this._bitfi.getSessionSecret()
+    const publicKey = this._bitfi.getPublicKey()
+    const config = this._bitfi.getConfig()
+
+    const serialized = {
+      authToken,
+      appSecret,
+      publicKey,
+      config
+    }
+
+    return serialized
+  }
+
+  async deserialize(obj) {
+    this._bitfi = await init(
+      obj.authToken, 
+      obj.publicKey, 
+      obj.appSecret,
+      obj.config
+    )
+  }
+
+  async addAccounts(n) {
+    throw new Error("Is not supported on this device");
+  }
+
+  async getAccounts() {
+    const address = this._bitfi.getAddress()
+    return [address]
+  }
+
+  async signTransaction(address, transaction) {
+    this._checkAddress(address)
+
+    //if (transaction.data) {
+    const serialized = transaction.serialize().toString('hex')
+    const res = await this._bitfi.signMessageBlind(serialized)
+
+    const { v, r, s } = derh2obj(res.signatureResponse)
+    transaction.v = v
+    transaction.r = r
+    transaction.s = s
+    return transaction
+    //}
+  }
+
+  _checkAddress(address) {
+    if (address.toLowerCase() !== this._bitfi.getAddress().toLowerCase()) {
+      throw new Error(`This address is not present in bitfi wallet: ${address}`)
+    }
+  }
+
+  async signMessage(address, data) {
+    this._checkAddress(address)
+
+    const signature = await this._bitfi.signMessageBlind(data)
+    return signature.signatureResponse
+  }
+
+  async getEncryptionPublicKey(address) {
+    this._checkAddress(address)
+
+    return this._bitfi.getPublicKey()
+  }
+
+  async decryptMessage(address, data) {
+    throw new Error("Method not implemented.");
+  }
+
+  async exportAccount(address) {
+    throw new Error("Not supported on this device");
+  }
+
+  async removeAccount(address) {
+    this._bitfi = null
+  }
+  
+}
+
+BitfiKeyring.type = "BitfiKeyring"
+module.exports = {
+  BitfiKeyring,
+  signin, 
+  calculateCode
+}
